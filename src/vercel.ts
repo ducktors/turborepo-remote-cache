07f67bb17a7682aae2cee633b0e512cdaad0a601
Maksim Sinik <maksim@sinik.it>
2022-01-12 19:06:30 +0100

    chore: adds vercel types
    uses node 14

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,8 +1,8 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { createApp } from './app'
-
 const app = createApp()
 
-export default async (req, res) => {
+export default async function (req: VercelRequest, res: VercelResponse) {
   await app.ready()
   app.server.emit('request', req, res)
 }
