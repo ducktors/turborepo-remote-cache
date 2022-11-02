25c70fa74d119722571849ec7bbd8f3878ffa3af
Andrew Nagy <anagy@clearlyip.com>
2022-11-02 22:11:21 +0000

    make optional

--- src/env.ts
+++ src/env.ts
@@ -24,7 +24,7 @@ const schema = Type.Object(
     STORAGE_PROVIDER: Type.Optional(
       Type.Enum(STORAGE_PROVIDERS, { default: STORAGE_PROVIDERS.LOCAL }),
     ),
-    BODY_LIMIT: Type.Number({ default: 104857600 }),
+    BODY_LIMIT: Type.Optional(Type.Number({ default: 104857600 })),
     STORAGE_PATH: Type.Optional(Type.String()),
     STORAGE_PATH_USE_TMP_FOLDER: Type.Optional(Type.Boolean({ default: true })),
     // AWS_ env vars are used as aws-sdk defaults
