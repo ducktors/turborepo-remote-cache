2f0b9c07834b7798dba14b1475eb4420a52c325b
Matteo Vivona <matteo.vivona@me.com>
2023-08-24 15:48:22 +0200

    feat: allow logging on file

--- src/env.ts
+++ src/env.ts
@@ -22,6 +22,8 @@ const schema = Type.Object(
     TURBO_TOKEN: Type.String({ separator: ',' }),
     PORT: Type.Number({ default: 3000 }),
     LOG_LEVEL: Type.Optional(Type.String({ default: 'info' })),
+    LOG_TYPE: Type.Optional(Type.String({ default: 'stdout' })),
+    LOG_DIR: Type.Optional(Type.String({ default: 'server.log' })),
     STORAGE_PROVIDER: Type.Optional(
       Type.Enum(STORAGE_PROVIDERS, { default: STORAGE_PROVIDERS.LOCAL }),
     ),
--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,6 @@
 import Pino from 'pino'
 import { env } from './env'
+
 const PinoLevelToSeverityLookup = {
   trace: 'DEBUG',
   debug: 'DEBUG',
@@ -9,18 +10,31 @@ const PinoLevelToSeverityLookup = {
   fatal: 'CRITICAL',
 } as const
 
-export const logger = Pino({
-  level: env.LOG_LEVEL,
-  messageKey: 'message',
-  redact: ['req.headers.authorization'],
-  formatters: {
-    level(label, number) {
-      return {
-        severity: PinoLevelToSeverityLookup[label] || PinoLevelToSeverityLookup['info'],
-        level: number,
-      }
+let logDestination
+
+if (env.LOG_TYPE === 'file') {
+  logDestination = Pino.destination(env.LOG_DIR)
+} else if (env.LOG_TYPE === 'stdout') {
+  logDestination = process.stdout
+} else {
+  throw new Error('Invalid LOG_TYPE. Allowed values are "file" or "stdout".')
+}
+
+export const logger = Pino(
+  {
+    level: env.LOG_LEVEL,
+    messageKey: 'message',
+    redact: ['req.headers.authorization'],
+    formatters: {
+      level(label, number) {
+        return {
+          severity: PinoLevelToSeverityLookup[label] || PinoLevelToSeverityLookup['info'],
+          level: number,
+        }
+      },
     },
   },
-})
+  logDestination,
+)
 
 export default logger
