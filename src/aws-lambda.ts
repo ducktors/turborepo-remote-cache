c028429994d805c87598a78068ab5cbc3a7cfa97
Maksim Sinik <maksim@sinik.it>
2023-09-12 16:40:57 +0200

    chore: remove eslint

--- src/aws-lambda.ts
+++ src/aws-lambda.ts
@@ -5,5 +5,4 @@ const app = createApp({
   trustProxy: true,
 })
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler = awsLambdaFastify(app, { enforceBase64: _ => true })
