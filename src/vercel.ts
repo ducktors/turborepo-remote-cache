8f9d5bb1bca9c583fa7d698148e0f96bd81cc2b1
Maksim Sinik <maksim@sinik.it>
2023-09-22 23:00:12 +0200

    fix: update Vercel function handler

--- src/vercel.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { createApp } from './app.js'
-
-const app = createApp()
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-  await app.ready()
-  app.server.emit('request', req, res)
-}
