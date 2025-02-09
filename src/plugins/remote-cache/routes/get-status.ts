cbc5e233db9a8bb4d1a9ff96e752e1f2c5f4b1b6
Clayton Davis <claytondus@gmail.com>
2025-02-08 22:01:20 -0500

    fix: import package.json to work with bundlers

--- src/plugins/remote-cache/routes/get-status.ts
+++ src/plugins/remote-cache/routes/get-status.ts
@@ -1,21 +1,12 @@
-import { readFileSync } from 'fs'
 import type { Server } from 'http'
-import { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
 import type {
   RawReplyDefaultExpression,
   RawRequestDefaultExpression,
   RouteOptions,
 } from 'fastify'
+import packageJson from '../../../../package.json'
 import { type Params, type Querystring } from './schema.js'
 import { statusRouteSchema } from './status-schema.js'
-
-// Get package.json version
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-const packageJson = JSON.parse(
-  readFileSync(join(__dirname, '../../../../package.json'), 'utf8'),
-)
 
 export const getStatus: RouteOptions<
   Server,
