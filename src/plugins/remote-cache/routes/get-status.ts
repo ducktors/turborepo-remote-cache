ad63d6e057c883b888065139e037a2401186395f
Matteo Vivona <matteo.vivona@me.com>
2025-02-11 15:14:45 +0100

    fix: remove package.json import on status page

--- src/plugins/remote-cache/routes/get-status.ts
+++ src/plugins/remote-cache/routes/get-status.ts
@@ -1,7 +1,4 @@
-import { readFileSync } from 'fs'
 import type { Server } from 'http'
-import { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
 import type {
   RawReplyDefaultExpression,
   RawRequestDefaultExpression,
@@ -9,13 +6,6 @@ import type {
 } from 'fastify'
 import { type Params, type Querystring } from './schema.js'
 import { statusRouteSchema } from './status-schema.js'
-
-// Get package.json version
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-const packageJson = JSON.parse(
-  readFileSync(join(__dirname, '../../../../package.json'), 'utf8'),
-)
 
 export const getStatus: RouteOptions<
   Server,
@@ -33,7 +23,7 @@ export const getStatus: RouteOptions<
   async handler(req, reply) {
     reply.send({
       status: 'enabled',
-      version: packageJson.version,
+      version: process.env.npm_package_version ?? 'unknown',
     })
   },
 }
