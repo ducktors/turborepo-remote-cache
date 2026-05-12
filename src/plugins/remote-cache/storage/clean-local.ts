f96b4974a26ed7e4ff9150e46245de550a3503db
Konstantin Modin <konstantin.modin@check24.de>
2026-05-12 13:55:28 +0200

    feat: add local cache cleanup endpoint

---
+++ src/plugins/remote-cache/storage/clean-local.ts
@@ -0,0 +1,46 @@
+import { readdir, stat, unlink } from 'node:fs/promises'
+import { join } from 'node:path'
+
+const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000
+
+export type CleanStats = {
+  deleted: number
+  scanned: number
+}
+
+export async function cleanStaleLocalArtifacts(
+  rootPath: string,
+  team: string,
+  olderThanDays: number,
+): Promise<CleanStats> {
+  const teamPath = join(rootPath, team)
+  const cutoff = Date.now() - olderThanDays * MILLISECONDS_PER_DAY
+  let deleted = 0
+  let scanned = 0
+
+  let entries: string[]
+  try {
+    entries = await readdir(teamPath)
+  } catch (err) {
+    if ((err as NodeJS.ErrnoException).code === 'ENOENT') {
+      return { deleted, scanned }
+    }
+    throw err
+  }
+
+  for (const entry of entries) {
+    const filePath = join(teamPath, entry)
+    const fileStats = await stat(filePath)
+    if (!fileStats.isFile()) {
+      continue
+    }
+
+    scanned++
+    if (fileStats.atimeMs <= cutoff) {
+      await unlink(filePath)
+      deleted++
+    }
+  }
+
+  return { deleted, scanned }
+}
